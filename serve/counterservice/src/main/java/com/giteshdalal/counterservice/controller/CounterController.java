ee116c4deddb43c0ad43c84e1069039ffeb75c7d
Gitesh <giteshdalal@gmail.com>
2018-04-16 14:15:12 +1200

    Initialise Serve Microservices Project

---
+++ serve/counterservice/src/main/java/com/giteshdalal/counterservice/controller/CounterController.java
@@ -0,0 +1,24 @@
+package com.giteshdalal.counterservice.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class CounterController {
+
+    private static AtomicLong count = new AtomicLong(0);
+
+    @Value("${counter.prefixMessage: Hello Default}")
+    private String prefixMessage;
+
+    @GetMapping(path = "/count")
+    public String getCount() {
+        return prefixMessage + count.getAndIncrement();
+    }
+}
+
