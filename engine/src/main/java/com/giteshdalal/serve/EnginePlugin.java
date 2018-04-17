43a9c9688754133fd0f4c9e260c6d2f029852a8f
Gitesh <giteshdalal@gmail.com>
2018-04-18 08:56:08 +1200

    Added Plugin Task to BUILD group for gradle

--- engine/src/main/java/com/giteshdalal/serve/EnginePlugin.java
+++ engine/src/main/java/com/giteshdalal/serve/EnginePlugin.java
@@ -2,14 +2,18 @@ package com.giteshdalal.serve;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 
 public class EnginePlugin implements Plugin<Project> {
-    public void apply(Project project) {
-        project.getTasks().create("generateFiles", GenerateFilesTask.class, t -> {
-            t.setGensrc(project.file(EnginePluginConstants.GENSRC));
-            t.setResources(project.file(EnginePluginConstants.RESOURCES));
-            t.setTemplates(project.file(EnginePluginConstants.TEMPLATES));
-            t.setYmlSearchRegex(EnginePluginConstants.YML_SEARCH_REGEX);
-        });
-    }
+	public void apply(Project project) {
+		Task serveTask = project.getTasks().create("serve", GenerateFilesTask.class, t -> {
+			t.setGensrc(project.file(EnginePluginConstants.GENSRC));
+			t.setResources(project.file(EnginePluginConstants.RESOURCES));
+			t.setTemplates(project.file(EnginePluginConstants.TEMPLATES));
+			t.setYmlSearchRegex(EnginePluginConstants.YML_SEARCH_REGEX);
+		});
+		serveTask.setGroup("build");
+		serveTask.setDescription(
+				"Generates models, respositories and resources in gensrc folder using *models.yml files");
+	}
 }
