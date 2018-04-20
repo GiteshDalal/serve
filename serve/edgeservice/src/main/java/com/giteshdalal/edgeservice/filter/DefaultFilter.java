a402337996aaec97e4a9f4a90fee87c6c611b708
Gitesh <giteshdalal@gmail.com>
2018-04-20 16:30:55 +1200

    Added Edge Service

---
+++ serve/edgeservice/src/main/java/com/giteshdalal/edgeservice/filter/DefaultFilter.java
@@ -0,0 +1,39 @@
+package com.giteshdalal.edgeservice.filter;
+
+import javax.servlet.http.HttpServletRequest;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.ZuulFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultFilter extends ZuulFilter {
+
+	private static Logger log = LoggerFactory.getLogger(DefaultFilter.class);
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+
+		log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+		return null;
+	}
+
+}
