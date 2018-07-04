4f744682eba4d1556c253ddc52f2cb61a5ace752
Gitesh <giteshdalal@gmail.com>
2018-07-04 23:29:36 +1200

    Fixed Bug related to Vault

--- serve/vault/init.sh
+++ serve/vault/init.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+if [ -n "$1" ]
+then
+    VAULT_DEV_TOKEN="$1"
+    CONFIG_DIR="."
+fi
+
 vault login ${VAULT_DEV_TOKEN}
+
+vault secrets disable secret/
+vault secrets enable -path=secret -version=1 kv
 
 vault kv put secret/auth-service/dev @${CONFIG_DIR}/auth-service-credentials-dev.json
 vault kv put secret/auth-service/local @${CONFIG_DIR}/auth-service-credentials-local.json
